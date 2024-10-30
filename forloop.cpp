15eaa865ae2399683f10b4854aee9881c378abab
Ryan Khieu <159482357+RyanKhieu@users.noreply.github.com>
2024-10-30 08:15:10 -0400

    added third bullet

--- forloop.cpp
+++ forloop.cpp
@@ -4,5 +4,11 @@ int main() {
     for (int i = 2; i <= 10; i += 2) {
         std::cout << i << std::endl;
     }
+
+    std::cout << std::endl;
+
+    for (int i = 10; i >= 2; i -= 2) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
